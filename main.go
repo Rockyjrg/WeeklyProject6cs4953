731e45de6818109dc8ebdf158bdb1bcf849b0a1a
unknown <jamie.g.ii@hotmail.com>
2025-03-07 11:59:38 -0600

    Got a moveable board made that is constrained to the screen left and right, next find out how to get the ball to start on the top of the said board.

--- main.go
+++ main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
+type PlayerBoard struct {
+	xpos   float32
+	ypos   float32
+	speed  float32
+	width  float32
+	height float32
+	color  rl.Color
+}
+
+func NewBoard(xpos, ypos, speed, width, height float32, color rl.Color) PlayerBoard {
+	return PlayerBoard{
+		xpos:   xpos,
+		ypos:   ypos,
+		speed:  speed,
+		width:  width,
+		height: height,
+		color:  color,
+	}
+}
+
+func (c PlayerBoard) DrawBoard() {
+	rl.DrawRectangle(int32(c.xpos), int32(c.ypos), int32(c.width), int32(c.height), c.color)
+}
+
+func (c *PlayerBoard) PlayerMovement(xOffset float32) {
+	c.xpos += xOffset
+
+	if c.xpos < 0 {
+		c.xpos = 0
+	}
+	if c.xpos+c.width > float32(rl.GetScreenWidth()) {
+		c.xpos = float32(rl.GetScreenWidth()) - c.width
+	}
+}
+
+func main() {
+	rl.InitWindow(800, 400, "Weekly Project 6 - Breakout Game")
+
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	//create the moving board
+	player := NewBoard(400, 350, 10, 60, 5, rl.RayWhite)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.DarkBlue)
+
+		//draw the board player will be moving
+		player.DrawBoard()
+
+		if rl.IsKeyDown(rl.KeyA) {
+			player.PlayerMovement(-player.speed)
+		}
+		if rl.IsKeyDown(rl.KeyD) {
+			player.PlayerMovement(player.speed)
+		}
+
+		rl.EndDrawing()
+	}
 }
