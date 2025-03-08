b7c8ace863d28da87da738bdf9d133259bab657d
unknown <jamie.g.ii@hotmail.com>
2025-03-08 04:23:24 -0600

    Very scuffed but working game. Ball destroys blocks if it collides with it. Also bounces correctly and speeds up a little bit on hit. Kind of bad collision detection with the paddle but that is fine for now.

--- main.go
+++ main.go
@@ -13,6 +13,15 @@ type PlayerBoard struct {
 	color  rl.Color
 }
 
+// struct for the blocks that will make a grid
+type Block struct {
+	Pos    rl.Vector2
+	Width  float32
+	Height float32
+	Color  rl.Color
+	Active bool //block has been hit or not
+}
+
 // physics for the ball
 type PhysicsBody struct {
 	Pos    rl.Vector2
@@ -21,6 +30,7 @@ type PhysicsBody struct {
 	Radius float32
 }
 
+// create a new paddle
 func NewBoard(xpos, ypos, speed, width, height float32, color rl.Color) PlayerBoard {
 	return PlayerBoard{
 		xpos:   xpos,
@@ -61,6 +71,49 @@ func (b PhysicsBody) DrawBall() {
 	rl.DrawCircle(int32(b.Pos.X), int32(b.Pos.Y), b.Radius, rl.RayWhite)
 }
 
+// function to create a grid of blocks
+func CreateBlocks(rows, cols int, blockWidth, blockHeight, spacing float32) []Block {
+	var blocks []Block
+	startX := float32(50) //top padding
+	startY := float32(50) //bottom padding
+
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			x := startX + float32(col)*(blockWidth+spacing)
+			y := startY + float32(row)*(blockHeight+spacing)
+			block := Block{
+				Pos:    rl.NewVector2(x, y),
+				Width:  blockWidth,
+				Height: blockHeight,
+				Color:  rl.Orange,
+				Active: true,
+			}
+			blocks = append(blocks, block)
+		}
+	}
+	return blocks
+}
+
+// function to draw the blocks we made
+func (b Block) DrawBlock() {
+	if b.Active {
+		rl.DrawRectangle(int32(b.Pos.X), int32(b.Pos.Y), int32(b.Width), int32(b.Height), b.Color)
+	}
+}
+
+// function to check if ball collided with a square
+func BallCollision(ball *PhysicsBody, block *Block) bool {
+	if !block.Active {
+		return false
+	}
+
+	//AABB collision check(axis-aligned bounding box)
+	return ball.Pos.X+ball.Radius > block.Pos.X &&
+		ball.Pos.X-ball.Radius < block.Pos.X+block.Width &&
+		ball.Pos.Y+ball.Radius > block.Pos.Y &&
+		ball.Pos.Y-ball.Radius < block.Pos.Y+block.Height
+}
+
 func main() {
 	rl.InitWindow(800, 400, "Weekly Project 6 - Breakout Game")
 
@@ -72,8 +125,10 @@ func main() {
 	player := NewBoard(400, 350, 5, 60, 5, rl.RayWhite)
 	//initialize a ball
 	ball := NewBall(player.xpos+player.width/2, player.ypos-10)
+	//draw the grid of blocks
+	blocks := CreateBlocks(3, 11, 60, 50, 5)
 
-	ballLaunched := false
+	ballLaunched := false //if ball has been launched or not
 
 	//start game loop
 	for !rl.WindowShouldClose() {
@@ -101,7 +156,13 @@ func main() {
 		//use space to launch the static ball off the paddle
 		if rl.IsKeyPressed(rl.KeySpace) && !ballLaunched {
 			ball.Vel.Y = -3
-			ball.Vel.X = player.speed * 0.5
+			if rl.IsKeyDown(rl.KeyA) {
+				ball.Vel.X = -2 // Move left if A is held
+			} else if rl.IsKeyDown(rl.KeyD) {
+				ball.Vel.X = 2 // Move right if D is held
+			} else {
+				ball.Vel.X = 0 // Go straight up if no key is held
+			}
 			ballLaunched = true
 		}
 
@@ -122,6 +183,7 @@ func main() {
 			ball = NewBall(player.xpos+player.width/2, player.ypos-10)
 			ball.Vel = rl.NewVector2(0, 0) //reset velocity of ball
 			ballLaunched = false
+			blocks = CreateBlocks(3, 11, 60, 50, 5) // Reset blocks
 		}
 
 		//bounce off the paddle
@@ -129,7 +191,7 @@ func main() {
 			ball.Pos.X > player.xpos &&
 			ball.Pos.X < player.xpos+player.width {
 
-			ball.Vel.Y = -1
+			ball.Vel.Y = -3
 
 			hitPosition := (ball.Pos.X - player.xpos) / player.width
 			ball.Vel.X = (hitPosition - 0.5) * 6
@@ -137,6 +199,55 @@ func main() {
 
 		//draw the ball
 		ball.DrawBall()
+
+		//draw all the blocks
+		for i := range blocks {
+			blocks[i].DrawBlock()
+		}
+
+		//check through each block to see if it was collided with
+		for i := range blocks {
+			//blocks[i].DrawBlock()
+			if !blocks[i].Active {
+				continue
+			}
+
+			if BallCollision(&ball, &blocks[i]) {
+				blocks[i].Active = false //remove block
+				// //determine bounce direction
+				block := blocks[i]
+
+				//check which side the ball hit
+				if ball.Pos.Y < block.Pos.Y { //hit from below
+					ball.Vel.Y = -ball.Vel.Y
+				} else if ball.Pos.Y > block.Pos.Y+block.Height { //hit from above
+					ball.Vel.Y = -ball.Vel.Y
+				} else if ball.Pos.X < block.Pos.X { //hit from left
+					ball.Vel.X = -ball.Vel.X
+				} else if ball.Pos.X > block.Pos.X+block.Width { //hit from right
+					ball.Vel.X = -ball.Vel.X
+				}
+				//increase ball speed slightly
+				ball.Vel.X *= 1.05
+				ball.Vel.Y *= 1.05
+			}
+		}
+
+		//reset game
+		gameOver := true
+		for _, block := range blocks {
+			if block.Active {
+				gameOver = false
+				break
+			}
+		}
+
+		if gameOver {
+			ball = NewBall(player.xpos+player.width/2, player.ypos-10)
+			ball.Vel = rl.NewVector2(0, 0)
+			ballLaunched = false
+			blocks = CreateBlocks(3, 11, 60, 50, 5)
+		}
 
 		rl.EndDrawing()
 	}
