c24a10f59aafca2d6eb2ec02dd4e9712c248f954
unknown <jamie.g.ii@hotmail.com>
2025-03-07 22:43:34 -0600

    Got ball to bounce off paddle and stay in constraints of the screen. Velocity on the ball hitting the paddle is a little weird. Need to add cubes in the middle and fix collision logic.

--- main.go
+++ main.go
@@ -13,6 +13,14 @@ type PlayerBoard struct {
 	color  rl.Color
 }
 
+// physics for the ball
+type PhysicsBody struct {
+	Pos    rl.Vector2
+	Vel    rl.Vector2
+	Mass   float32
+	Radius float32
+}
+
 func NewBoard(xpos, ypos, speed, width, height float32, color rl.Color) PlayerBoard {
 	return PlayerBoard{
 		xpos:   xpos,
@@ -24,6 +32,7 @@ func NewBoard(xpos, ypos, speed, width, height float32, color rl.Color) PlayerBo
 	}
 }
 
+// function to draw the board using PlayerBoard struct
 func (c PlayerBoard) DrawBoard() {
 	rl.DrawRectangle(int32(c.xpos), int32(c.ypos), int32(c.width), int32(c.height), c.color)
 }
@@ -39,6 +48,19 @@ func (c *PlayerBoard) PlayerMovement(xOffset float32) {
 	}
 }
 
+func NewBall(x, y float32) PhysicsBody {
+	return PhysicsBody{
+		Pos:    rl.NewVector2(x, y),
+		Vel:    rl.NewVector2(0, 0),
+		Mass:   1,
+		Radius: 5,
+	}
+}
+
+func (b PhysicsBody) DrawBall() {
+	rl.DrawCircle(int32(b.Pos.X), int32(b.Pos.Y), b.Radius, rl.RayWhite)
+}
+
 func main() {
 	rl.InitWindow(800, 400, "Weekly Project 6 - Breakout Game")
 
@@ -47,8 +69,13 @@ func main() {
 	rl.SetTargetFPS(60)
 
 	//create the moving board
-	player := NewBoard(400, 350, 10, 60, 5, rl.RayWhite)
+	player := NewBoard(400, 350, 5, 60, 5, rl.RayWhite)
+	//initialize a ball
+	ball := NewBall(player.xpos+player.width/2, player.ypos-10)
 
+	ballLaunched := false
+
+	//start game loop
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
@@ -57,12 +84,59 @@ func main() {
 		//draw the board player will be moving
 		player.DrawBoard()
 
+		//movement keys for the paddle
 		if rl.IsKeyDown(rl.KeyA) {
 			player.PlayerMovement(-player.speed)
 		}
 		if rl.IsKeyDown(rl.KeyD) {
 			player.PlayerMovement(player.speed)
 		}
+
+		//make sure the ball starts above the paddle board
+		if !ballLaunched {
+			ball.Pos.X = player.xpos + player.width/2
+			ball.Pos.Y = player.ypos - ball.Radius
+		}
+
+		//use space to launch the static ball off the paddle
+		if rl.IsKeyPressed(rl.KeySpace) && !ballLaunched {
+			ball.Vel.Y = -3
+			ball.Vel.X = player.speed * 0.5
+			ballLaunched = true
+		}
+
+		//once ball is moving update its position
+		ball.Pos.X += ball.Vel.X
+		ball.Pos.Y += ball.Vel.Y
+
+		//bounce off walls
+		if ball.Pos.X-ball.Radius <= 0 || ball.Pos.X+ball.Radius >= float32(rl.GetScreenWidth()) {
+			ball.Vel.X *= -1 //reverse the direction
+		}
+		if ball.Pos.Y-ball.Radius <= 0 {
+			ball.Vel.Y *= -1 //reverse the direction if the ball hits the top
+		}
+
+		//check to see if ball falls below the screen
+		if ball.Pos.Y > float32(rl.GetScreenHeight()) {
+			ball = NewBall(player.xpos+player.width/2, player.ypos-10)
+			ball.Vel = rl.NewVector2(0, 0) //reset velocity of ball
+			ballLaunched = false
+		}
+
+		//bounce off the paddle
+		if ball.Pos.Y+ball.Radius >= player.ypos &&
+			ball.Pos.X > player.xpos &&
+			ball.Pos.X < player.xpos+player.width {
+
+			ball.Vel.Y = -1
+
+			hitPosition := (ball.Pos.X - player.xpos) / player.width
+			ball.Vel.X = (hitPosition - 0.5) * 6
+		}
+
+		//draw the ball
+		ball.DrawBall()
 
 		rl.EndDrawing()
 	}
